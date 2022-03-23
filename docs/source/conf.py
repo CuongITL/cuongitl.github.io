556f18f2a1605f736946d77a8cd63bbb1ea9c248
CuongITL <77845990+CuongITL@users.noreply.github.com>
2022-03-23 10:39:54 +0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,6 +23,7 @@ intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
+github_url = "https://t.me/Cuongitl"
 intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
